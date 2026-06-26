6439c5ea4e3f5ea72a9c11704547d4ed613fde73
Bryan Wills <bryanwills2008@gmail.com>
2026-06-26 19:20:05 +0530

    Yay...

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,7 @@
 const config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    '@tailwindcss/postcss': {},
   },
-  plugins: [require("daisyui")], // <-- Add daisyUI here
 };
+
 export default config;
